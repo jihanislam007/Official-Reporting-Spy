0076e4908359dadc909e206b283448b9a3a5b947
jihan <jihanislam007@example.com>
2017-11-07 16:20:30 +0600

    Merge branch 'master' of https://github.com/jihanislam007/Reportings
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/LogInActivity.java

--- app/src/main/java/mazharul/islam/jihan/reportings_spy/Activity/SuperAdmin/ClientAdminControlActivity.java
+++ app/src/main/java/mazharul/islam/jihan/reportings_spy/Activity/SuperAdmin/ClientAdminControlActivity.java
@@ -3,6 +3,8 @@ package mazharul.islam.jihan.reportings_spy.Activity.SuperAdmin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,10 +30,18 @@ public class ClientAdminControlActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_admin_control);
 
-        AdminEmailAddressEditText = (EditText) findViewById(R.id.AdminEmailAddressEditText);
-        SenderDoneButton = (Button) findViewById(R.id.SenderDoneButton);
+       //// AdminEmailAddressEditText = (EditText) findViewById(R.id.AdminEmailAddressEditText);
+       // SenderDoneButton = (Button) findViewById(R.id.SenderDoneButton);
         StopAdminButton = (Button) findViewById(R.id.StopAdminButton);
-        DeleteDataButton = (Button) findViewById(R.id.DeleteDataButton);
+       // DeleteDataButton = (Button) findViewById(R.id.DeleteDataButton);
+
+        StopAdminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Animation myAnim = AnimationUtils.loadAnimation(ClientAdminControlActivity.this, R.anim.bounce);
+                StopAdminButton.startAnimation(myAnim);
+            }
+        });
 
 
     }
