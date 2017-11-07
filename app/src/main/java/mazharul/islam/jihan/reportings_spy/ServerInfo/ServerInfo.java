8bab625360eb3ed8aad699c1206c2696daf30ff3
ar13101085 <ar13101085@gmail.com>
2017-11-07 21:16:00 +0600

    Signed-off-by: ar13101085 <ar13101085@gmail.com>

--- app/src/main/java/mazharul/islam/jihan/reportings_spy/ServerInfo/ServerInfo.java
+++ app/src/main/java/mazharul/islam/jihan/reportings_spy/ServerInfo/ServerInfo.java
@@ -5,9 +5,9 @@ package mazharul.islam.jihan.reportings_spy.ServerInfo;
  */
 
 public class ServerInfo {
-    /*public static String BASE_URL="http://104.251.215.144:8587/";
-    public static String BASE_URL2="http://104.251.215.144:8587";*/
+    public static String BASE_URL="http://104.251.215.144:8587/";
+    public static String BASE_URL2="http://104.251.215.144:8587";
 
-    public static String BASE_URL="http://192.168.0.105:8000/";
-    public static String BASE_URL2="http://192.168.0.105:8000";
+    /*public static String BASE_URL="http://192.168.0.105:8000/";
+    public static String BASE_URL2="http://192.168.0.105:8000";*/
 }
