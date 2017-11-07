45a910707166e088df666094c8c2fb96538d6225
jihan <jihanislam007@example.com>
2017-11-07 15:45:07 +0600

    Merge branch 'master' of https://github.com/jihanislam007/Reportings
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/LogInActivity.java

--- app/src/main/java/mazharul/islam/jihan/reportings_spy/Activity/SpyActivity/SpyNewsViewActivity.java
+++ app/src/main/java/mazharul/islam/jihan/reportings_spy/Activity/SpyActivity/SpyNewsViewActivity.java
@@ -8,20 +8,33 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.baoyz.widget.PullRefreshLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+import cz.msebera.android.httpclient.Header;
 import mazharul.islam.jihan.reportings_spy.Activity.DetailsNewsViewActivity;
 import mazharul.islam.jihan.reportings_spy.Activity.SettingsActivity;
 import mazharul.islam.jihan.reportings_spy.Adapter.AdminListViewAdaptor;
 import mazharul.islam.jihan.reportings_spy.Adapter.SpyListAdopter;
 import mazharul.islam.jihan.reportings_spy.JsonModel.ReportListItem;
 import mazharul.islam.jihan.reportings_spy.R;
+import mazharul.islam.jihan.reportings_spy.ServerInfo.ServerInfo;
 
 public class SpyNewsViewActivity extends AppCompatActivity {
 
     ListView listView;
     ImageView setting;
     ArrayList<ReportListItem> reportListItems=new ArrayList<ReportListItem>();
+    PullRefreshLayout swipeRefreshLayout;
+    SpyListAdopter spyListAdopter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +53,18 @@ public class SpyNewsViewActivity extends AppCompatActivity {
 
 
         ///////////////////listView/////////////////////////////
+
+        swipeRefreshLayout = (PullRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadReportList();
+
+            }
+        });
+
         listView = (ListView) findViewById(R.id.SpyNewsListView);
-        SpyListAdopter spyListAdopter = new SpyListAdopter(SpyNewsViewActivity.this, reportListItems);
+        spyListAdopter = new SpyListAdopter(SpyNewsViewActivity.this, reportListItems);
         listView.setAdapter(spyListAdopter);
 
 
@@ -51,6 +74,27 @@ public class SpyNewsViewActivity extends AppCompatActivity {
 
                 Intent in = new Intent(SpyNewsViewActivity.this , DetailsNewsViewActivity.class);
                 startActivity(in);
+            }
+        });
+    }
+
+    public void loadReportList(){
+        System.out.println("try to load server data");
+        AsyncHttpClient asyncHttpClient=new AsyncHttpClient();
+        asyncHttpClient.get(ServerInfo.BASE_URL+"GetAllReport/",new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                reportListItems.clear();
+                spyListAdopter.notifyDataSetChanged();
+                Gson gson=new Gson();
+                Type type = new TypeToken<ArrayList<ReportListItem>>() {}.getType();
+                ArrayList<ReportListItem> listItems=gson.fromJson(response.toString(),
+                        type);
+
+                reportListItems.addAll(listItems);
+                spyListAdopter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+
             }
         });
     }
